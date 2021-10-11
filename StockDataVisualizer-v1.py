eaf902a0190c104b5731ce95deba1f5f87a477b4
Amy Snell <60243135+b1ackkwid0w@users.noreply.github.com>
2021-10-11 13:35:16 -0500

    Create StockDataVisualizer-v1.py

---
+++ StockDataVisualizer-v1.py
@@ -0,0 +1,33 @@
+#started 10/11/2021 - Amy Snell
+
+# adding requirements and other info
+"""This program analyzes stock data and outputs to a webpage 
+client wants to track stock data trends from Alpha Vantage website. 
+This site offers an api that returns historical stock data from the past 20 years. 
+The data is returned in json format. There is no way to visualize the data or choose a date range to view the data. 
+The api, by default, returns 20 years of data for all but one of its functions.
+
+This program queries the Alpha Vantage api
+This program asks the user to select a date range to view the data, and the type of chart they want to view the data in
+
+This application should: 
+#Ask the user to enter the stock symbol for the company they want data for.
+#Ask the user for the chart type they would like.
+#Ask the user for the time series function they want the api to use.
+#Ask the user for the beginning date in YYYY-MM-DD format.
+#Ask the user for the end date in YYYY-MM-DD format.
+#The end date should not be before the begin date
+#Generate a graph and open in the user’s default browser.
+"""
+
+
+def main ():
+
+    print('Stock Data Visualizer\n======================')
+    
+    
+    
+    
+main()
+
+
